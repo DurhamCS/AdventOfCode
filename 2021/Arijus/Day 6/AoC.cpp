cda9ec80ca0f8e74bc161c0d8fec3f412123e59e
Arijus <aryjeleng@gmail.com>
2021-12-07 00:40:27 +0000

    updated Day 6

--- 2021/Arijus/Day 6/AoC.cpp
+++ 2021/Arijus/Day 6/AoC.cpp
@@ -8,32 +8,32 @@
 
 inline size_t key(int i,int j) {return (size_t) i << 32 | (unsigned int) j;}
 
-long double count_fish(int days, std::pair<int, int> fish, std::unordered_map<size_t, long double> &memo) {
+long double count_fish(int days, int fish, std::unordered_map<int, long double> &memo) {
     //count children
     //count children's children
     long double ans = 0;
-    int m = days-fish.first-fish.second;
+    int m = days-fish;
     if (m < 0) return 0;
     ans += m/7+1;
     for (int i = 0; i < m/7; i++) {
-        if ( memo.find(key(9, fish.first+fish.second+i*7)) == memo.end() ) {
-            ans += count_fish(days, std::make_pair(9, fish.first+fish.second+i*7), memo);
+        if ( memo.find(fish+i*7+9) == memo.end() ) {
+            ans += count_fish(days, fish+i*7+9, memo);
         } else {
-            ans += memo[key(9, fish.first+fish.second+i*7)];
+            ans += memo[fish+i*7+9];
         }
     }
-    memo[key(fish.first, fish.second)] = ans;
+    memo[fish] = ans;
     return ans;
 }
 
 int main() {
-    std::vector<std::pair<int,int>> fish;
+    std::vector<int> fish;
     std::ifstream fd("input.txt");
-    std::unordered_map<size_t, long double> memo;
+    std::unordered_map<int, long double> memo;
     std::string str;
     getline(fd, str);
     std::stringstream ss(str);
-    while (getline(ss, str, ',')) fish.push_back(std::make_pair(stoi(str)+1,0));
+    while (getline(ss, str, ',')) fish.push_back(stoi(str)+1);
     int n = fish.size();
     long double ans1 = n, ans2 = n;
     int number_of_days1 = 80, number_of_days2 = 256;
