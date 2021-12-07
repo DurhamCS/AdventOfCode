469314e43fefa1d08cef09f8e97b94248ec7d71e
Joe Clinton <48254978+Joeclinton1@users.noreply.github.com>
2021-12-07 03:16:31 +0000

    Remove unused count_temp

--- 2021/Joe/day6.cpp
+++ 2021/Joe/day6.cpp
@@ -11,9 +11,7 @@ typedef unsigned long long ull;
 
 ull f(vector<int> fishes, int days){
     ull counts[9] = {0};
-    ull counts_temp[9];
     for(const auto &fish: fishes) counts[fish] += 1;
-
     for(int d = 0; d<days; d++){
         ull temp = counts[0];
         for(int t = 0; t<8; t++) counts[t] = counts[t+1];
@@ -27,8 +25,7 @@ int main(){
     ifstream input("day6input.txt");
     string t;
     vector<int> fishes;
-    while (getline(input, t, ',')) fishes.push_back(stoi(t));
-
+    while (getline(input, t, ',')) fishes.push_back(stoi(t));_
     cout << f(fishes, 80) << '\n';
     cout << f(fishes, 256) << '\n';
 }   
