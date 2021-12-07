5515c75cf79d781c6310fc8f514d7f509647120b
Arijus <aryjeleng@gmail.com>
2021-12-07 15:57:20 +0000

    Day 7 update

--- 2021/Arijus/Day 7/AoC.cpp
+++ 2021/Arijus/Day 7/AoC.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <cmath>
 
 int main () {
     std::vector<int> pos;
@@ -13,27 +14,22 @@ int main () {
     while (getline(ss, str, ',')) pos.push_back(stoi(str));
 
     std::sort(pos.begin(), pos.end());
-
     int n = pos.size();
     int med = pos[n/2];
     int ans1 = 0;
-    long long int ans2 = 922337203685477580;
+    long long int ans2 = 0;
+    double e = 0;
 
     for (int i = 0; i < n; i++) {
         ans1 += abs(pos[i]-med);
+        e += pos[i];
     }
+    e = std::trunc(e/n);
 
-    for (int i = 0; i < pos[n-1]; i++) {
-        long long int mini = 0;
-        for (int j = 0; j < n; j++) {
-            med = abs(pos[j]-i);
-            mini += ((med+1)*med)/2;
-        }
-        if (ans2 > mini) {
-            ans2 = mini;
-        }
+    for (int i = 0; i < n; i++) {
+        ans2 += (abs(e-pos[i])+1)*abs(e-pos[i])/2;
+        // std::cout <<ans2 <<std::endl;
     }
-
 
     std::cout <<"Star 1: "<<ans1 <<std::endl
               <<"Star 2: "<<ans2 <<std::endl;
